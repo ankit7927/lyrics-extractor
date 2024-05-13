de08cc9494d929af5a21f226244f09472d10ee31
ankit <code7lancer@gmail.com>
2024-05-13 22:20:43 +0530

    validating upload url

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from lyricsmint import extractor
-import requests, time
-
-upload_url = ""
+import requests, time, sys
 
 if __name__=="__main__":
-    songs = []
+    upload_url = input("Enter upload url : ")
+    if upload_url == "":
+        print("exiting upload url is none")
+        sys.exit()
+
     request = requests.Session()
     with open("page_links.txt", "r") as file:
         for linke in file.read().splitlines():
             song_detail = extractor(request, linke)
-            songs.append(song_detail)
+
             time.sleep(2)
 
             request.post(url=upload_url, data=song_detail)
