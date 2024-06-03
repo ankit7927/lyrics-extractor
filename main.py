10970ca1097c3d11c570f0328a350b78664c032c
ankit <code7lancer@gmail.com>
2024-06-03 06:59:34 +0530

    added threading

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from lyricsmint import extractor
-import requests, sys
+import requests, sys, threading
 
-if __name__=="__main__":
-    upload_url = "http://localhost:8000/api/song/create"# input("Enter upload url : ")
-    if upload_url == "":
-        print("exiting upload url is none")
-        sys.exit()
+upload_url = "http://localhost:8000/api/song/create"# input("Enter upload url : ")
+if upload_url == "":
+    print("exiting upload url is none")
+    sys.exit()
 
-    request = requests.Session()
-    with open("page_links.txt", "r") as file:
-        for linke in file.read().splitlines():
-            try:
-                if linke == "":
-                    continue
-                song_detail = extractor(request, linke)
-                response = request.post(url=upload_url, json=song_detail)
-                print("uploaded -- " + str(response.status_code), "\n")
-            except Exception as e:
-                print(e)
+urls_list = []
+request = requests.Session()
 
-    request.close()
+with open("page_links.txt", "r") as file:
+    urls_list = file.read().splitlines()
+
+def extract():
+    while not len(urls_list) == 0 :
+        link = urls_list.pop()
+        try:
+            if link == "":
+                continue
+            song_detail = extractor(request, link)
+            response = request.post(url=upload_url, json=song_detail)
+            print("uploaded -- " + str(response.status_code), "\n")
+        except Exception as e:
+            print(e)
+            continue
+
+for _ in range(20):
+    threading.Thread(target=extract).start()
+    
